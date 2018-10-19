45a762894075f810ca808198ae5c01322fa0c384
M. D. Truong <truongmd@gmail.com>
2018-10-19 12:14:44 -0700

    Make a few changes to allow variables in the conf files

--- meta-evb/meta-evb-raspberrypi/meta-evb-raspberrypi-cm3/recipes-bsp/rpi-u-boot-scr/files/boot.cm3.cmd
+++ meta-evb/meta-evb-raspberrypi/meta-evb-raspberrypi-cm3/recipes-bsp/rpi-u-boot-scr/files/boot.cm3.cmd
@@ -1,4 +1,7 @@
+echo "Current dtb: $fdtfile"
+setenv fdtfile @@FDT_FILE@@
+echo "dtb redefined: $fdtfile"
 fatload mmc 0:1 ${kernel_addr_r} @@KERNEL_IMAGETYPE@@
-fatload mmc 0:1 0x2000000 bcm2710-rpi-cm3.dtb
+fatload mmc 0:1 0x2000000 $fdtfile
 setenv bootargs earlyprintk console=ttyAMA0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
 @@KERNEL_BOOTCMD@@ ${kernel_addr_r} - 0x2000000
--- meta-evb/meta-evb-raspberrypi/meta-evb-raspberrypi-cm3/recipes-bsp/rpi-u-boot-scr/rpi-u-boot-scr.bbappend
+++ meta-evb/meta-evb-raspberrypi/meta-evb-raspberrypi-cm3/recipes-bsp/rpi-u-boot-scr/rpi-u-boot-scr.bbappend
@@ -4,6 +4,7 @@ SRC_URI += "file://boot.cm3.cmd"
 do_compile() {
     sed -e 's/@@KERNEL_IMAGETYPE@@/${KERNEL_IMAGETYPE}/' \
         -e 's/@@KERNEL_BOOTCMD@@/${KERNEL_BOOTCMD}/' \
+        -e 's/@@FDT_FILE@@/${FDTFILE}/' \
         "${WORKDIR}/boot.cm3.cmd" > "${WORKDIR}/boot.cmd"
     mkimage -A arm -T script -C none -n "Boot script" -d "${WORKDIR}/boot.cmd" boot.scr
 }
